b485f88dbe8a0785c7eb8a2d70be01fd34a75b14
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-29 18:12:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var numero1 = parseInt(prompt("Digite um número"))
+var suc = numero1 + 1
+var ant = numero1 -1
+alert("O antecessor do número "+numero1+" é " + ant + " e o sucessor é "+suc)
+alert(`o sucessor é ${suc} e ${ant}`)
